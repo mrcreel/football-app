f9a38906311de962ac2d871c518198685fe612b4
mrcreel <michaelrcreel@gmail.com>
2023-03-14 12:06:21 -0500

    Commit rawData

---
+++ rawData/exports.js
@@ -0,0 +1,11 @@
+const schoolsData = require( './schools.json' )
+const seasonsData = require( './seasons.json' )
+const gamesData = require( './games.json' )
+
+console.log( schoolsData[ 0 ] )
+
+module.exports = {
+  schoolsData,
+  seasonsData,
+  gamesData
+}
