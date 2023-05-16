f1ff457dd6827be92f3f65117511921b65b79fa3
mrcreel <michaelrcreel@gmail.com>
2023-05-16 13:07:59 -0500

    feat: added rawData

---
+++ scraper/rawData/exports.js
@@ -0,0 +1,9 @@
+const schoolsData = require( './schools.json' )
+const seasonsData = require( './seasons.json' )
+const gamesData = require( './games.json' )
+
+module.exports = {
+  schoolsData,
+  seasonsData,
+  gamesData
+}
