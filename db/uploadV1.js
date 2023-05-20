c42234fd9f4deaee3262db734304824aa3dedd17
Michael R Creel <michaelrcreel@gmail.com>
2023-05-20 15:12:40 -0500

    feat: Upload v1 Seasons

--- db/uploadV1.js
+++ db/uploadV1.js
@@ -1,5 +1,5 @@
 require( 'dotenv' ).config()
-const {Schema, model, createConnection} = require( 'mongoose' )
+const { Schema, model, createConnection } = require( 'mongoose' )
 
 const MONGO_URI = process.env.MONGO_URI
 
@@ -14,110 +14,81 @@ const schoolSchema = new Schema( {
   name: { type: String, required: true },
   inState: { type: Boolean, required: true, default: true }
 } )
-const seasonV0Schema = new Schema( {
-  season: Number,
-  slug: String,
-  district: String,
-  divisionWins: Number,
-  divisionLosses: Number,
-  divisionTies: Number,
-  overallWins: Number,
-  overallLosses: Number,
-  overallTies: Number,
-  playoffWins: Number,
-  playoffLosses: Number,
-  teamPointsFor: Number,
-  teamPointsAgainst: Number,
-  teamPointsPerGameFor: Number,
-  teamPointsPerGameAgainst: Number
-} )
-const gameV0Schema = new Schema( {
-  gameSeason: Number,
-  gameSeasonWeek: Number,
-  gameDate: String,
-  gameTeamName: String,
-  gameTeamSlug: String,
-  gameTeamMascot: String,
-  gameLocation: String,
-  gameOpponentName: String,
-  gameisDistrictGame: Boolean,
-  gameResult: String,
-  gamePointsFor: Number,
-  gamePointsAgainst: Number
-} )
 
 
 const main = async () => {
-  const v0 = await createConnection(MONGO_URI+'v0?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, })
-  console.log('Connected to v0...')
-  const SchoolV0 = await v0.model( 'School', schoolSchema )
-  const SeasonV0 = await v0.model( 'Season', seasonV0Schema )
-  const GameV0 = await v0.model( 'Game', gameV0Schema )
-  // const schoolsV0 = await SchoolV0.find().sort({slug: 1})
+  const v0 = await createConnection( MONGO_URI + 'v0?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, } )
+  console.log( 'Connected to v0...' )
 
-  const v1 = await createConnection(MONGO_URI+'v1?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, })
-  console.log('Connected to v1...')
+
+  const v1 = await createConnection( MONGO_URI + 'v1?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, } )
+  console.log( 'Connected to v1...' )
+
+
   const SchoolV1 = await v1.model( 'School', schoolSchema )
+  let schoolsV1 = await SchoolV1.find().sort( { slug: 1 } )
 
-  /*
-  Add schools from v0 DB as is
-  schoolsV0.map(async (school, idx) => {
-    const newSchool = await new SchoolV1({
+
+  const seasonV1Schema = new Schema( {
+    slug: String,
+    season: Number,
+    district: String,
+    name: String,
+    mascot: String
+  } )
+  const Season = v1.model( 'Season', seasonV1Schema )
+
+
+  schoolsV1.map( async ( school, idx ) => {
+    const slug = school.slug
+    const seasons = await getSeasonsForSchool( slug )
+    console.log(`${slug}: ${seasons.length}`)
+    seasons.map( async ( seasonData, idx ) => {
+      const games = await getGamesForSchoolBySeason( slug, seasonData.season )
+      if ( games[ 0 ] !== undefined ) {
+        const season = {}
+        season.slug = seasonData.slug
+        season.season = seasonData.season
+        season.district = seasonData.district
+        season.name = games[ 0 ].gameTeamName
+        season.mascot = games[ 0 ].gameTeamMascot
+        const newSeason = new Season( season )
+        await newSeason.save()
+      }
+    } )
+  } )
+
+  // uploadSchools()
+}
+
+const getSeasonsForSchool = async ( slug ) => {
+  return seasonsData.filter( ( season, idx ) => {
+    return season.slug == slug
+  } )
+}
+const getGamesForSchoolBySeason = async ( slug, season ) => {
+  return gamesData.filter( ( game, idx ) => {
+    return game.gameTeamSlug == slug && game.gameSeason == season
+  } )
+}
+
+const uploadSchools = async () => {
+
+  const SchoolV0 = await v0.model( 'School', schoolSchema )
+  let schoolsV0 = await SchoolV0.find().sort( { slug: 1 } )
+  const SchoolV1 = await v1.model( 'School', schoolSchema )
+  // Add schools from v0 DB as is
+  schoolsV0.map( async ( school, idx ) => {
+    const newSchool = await new SchoolV1( {
       slug: school.slug,
       name: school.name,
       inState: school.inState
-    })
-    // await newSchool.save()
-  })
-  */
-
- const seasonV1Schema = new Schema({
-   slug: String,
-   season: Number,
-   district: String,
-   name: String,
-   mascot: String
-  })
-  const SeasonV1 = await v1.model( 'Season', seasonV1Schema )
-
-  let schoolsV1 = await SchoolV1.find().sort({slug: 1})
-  schoolsV1 = schoolsV1.slice(0, 2)
-
-  schoolsV1.map(async (school, idx) => {
-    const slug = school.slug
-    let seasons = await SeasonV0.find({slug: slug}).sort({season: 1})
-    seasons = seasons.slice(0,2)
-    await console.log(`${slug}: ${seasons.length}`)
-
-
-
-    seasons.map(async (season, idx) => {
-      const district = season.district
-      const games = await GameV0.find({gameTeamSlug: slug, gameSeason: season.season})
-      const name = games[0].gameTeamName
-
-      await console.log(name);
-      // console.log(game);
-      // const newSeason = await new SeasonV1({
-      //   slug,
-      //   season: season.season,
-      //   district,
-      //   name: game.gameTeamName,
-      //   mascot: game.gameTeamMascot
-      // })
-      // // await newSeason.save()
-      // // console.clear()
-      // console.log(newSeason)
-    })
-
-
-
-  })
-
-
-
+    } )
+    await newSchool.save()
+  } )
 }
 
+const uploadV1Seasons = async () => { }
 
 
 main().catch( ( error ) => {
