d2665b812b5e14137cd3decc1c87fac9daff436c
Michael R Creel <michaelrcreel@gmail.com>
2023-06-02 17:45:31 -0500

    feat: Add React front end

--- utils/chunkData.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = chunkData = async (array, numberOfChunks) => {
-
-  const chunkSize = Math.floor(array.length / numberOfChunks)
-  for(let i = 0; i <= numberOfChunks; i++){
-    clg
-  }
-}
